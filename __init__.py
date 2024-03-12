90a9e1ab288bb05a6215343aa7a5fba615de8a6e
Bereket Daniel Mezgebu <bereketdaniel@berekets-mbp.lan>
2024-03-11 22:19:24 -0500

    Test 1

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from dotenv import load_dotenv
 import os
-dotenv_path = os.path.join(os.path.dirname(__file__), 'info.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), 'notes.env')
 load_dotenv(dotenv_path=dotenv_path)
